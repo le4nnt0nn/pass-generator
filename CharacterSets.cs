3333fdffb95284d3763fe2b9d50e41a5e6256387
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2025-01-01 12:24:55 +0100

    feat : refactor code and add class and controlls

---
+++ CharacterSets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGenerator
+{
+    public static class CharacterSets
+    {
+        public const string numbers = "0123456789";
+        public const string mayusLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string minusLetters = "abcdefghijklmnopqrstuvwxyz";
+        public const string specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/";
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,18 +23,47 @@ namespace PassGenerator
         public MainWindow()
         {
             InitializeComponent();
+            sliderLong.ValueChanged += SliderLong_ValueChanged;
+        }
+
+        // calculate if slider changed the value
+        private void SliderLong_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtLong.Text = ((int)sliderLong.Value).ToString();
         }
 
         private void btnGen_Click(object sender, EventArgs e)
         {
-            // base char
-            string numbers = "0123456789";
-            string mayusLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string minusLetters = "abcdefghijklmnopqrstuvwxyz";
-            string specialChars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/";
-
             // create characters group
             StringBuilder cgroup = new StringBuilder();
+
+            // add options if check is marked
+            cgroup.Append(string.Concat(chkNumbers.IsChecked == true ? CharacterSets.numbers : "", chkMayus.IsChecked == true ? CharacterSets.mayusLetters : "",
+            chkMinus.IsChecked == true ? CharacterSets.minusLetters : "", chkSpecials.IsChecked == true ? CharacterSets.specialChars : ""));
+
+            // check if all selection is off
+            if (cgroup.Length == 0) { MessageBox.Show("Select 1 option to continue.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning); return; }
+        }
+
+        private string generatePass(int lon, string charactersGroup)
+        {
+            StringBuilder passw = new StringBuilder();
+            Random r = new Random();
+
+            for (int i = 0; i < lon; i++)
+            {
+                int idx = r.Next(charactersGroup.Length);
+                passw.Append(charactersGroup[idx]);
+            }
+            return passw.ToString();
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtPass.Text))
+            {
+                Clipboard
+            }
         }
     }
 }
